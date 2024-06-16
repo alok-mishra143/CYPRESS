93df6e256e0288231a4abe279b1eefbe488211e3
alok_mishra14 <143alokmishra143@gmail.com>
2024-06-16 14:13:25 +0530

    analytic in website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 import { ThemeProvider } from "@/lib/provider/next-theme-provider";
 import AppStateProvider from "@/lib/provider/state-provider";
@@ -35,6 +36,7 @@ export default function RootLayout({
           >
             <SupabaseUserProvider>
               <SocketProvider>
+                <Analytics />
                 {children}
                 <Toaster />
               </SocketProvider>
